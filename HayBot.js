ef06e7c7c3eaa475d579b3065a7d5ff376ccb143
Kevin <pitok236@gmail.com>
2015-05-13 19:23:15 -0400

    Not sure what this is
    but alright

--- HayBot.js
+++ HayBot.js
@@ -1,5 +1,5 @@
 var irc = require('twitch-irc');
- 
+
 // Calling a new client.. 
 var client = new irc.client({
     options: {
@@ -10,7 +10,7 @@ var client = new irc.client({
     },
     identity: {
         username: 'HayBot',
-        password: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+        password: 'oauth:xxxxxxxxxxxXXXXXXXXXXxxxx'
     },
     channels: ['hay1tsme']
 });
@@ -31,29 +31,58 @@ client.addListener('disconnected', function (reason) {
 client.addListener('chat', function (channel, user, message) {
     // If the message starts with !hello.. 
     if (message.indexOf('!hello') === 0) {
-        // Say something 
-        // https://github.com/Schmoopiie/twitch-irc/wiki/Command:-Say 
-        client.say(channel, "Sup dude? Hows it going?");
+        // Greet the user
+        client.say(channel, "Sup dude, Hows it going? OpiOP /");
     }
 });
+//Help command
+cliant.addListener('chat', function (channel, user, message) {
+    //if the message starts with !help...
+    if (message.indexOf('!help') === 0) {
+        //link the user to the Haybot page on Hay1tsme.com
+        cliant.say(channel, "Help for HayBot can be found at https://hay1tsme.com/HayBot");
+    }
+}
 
 //Love you foxy <3
 client.addListener('chat', function (channel, user, message) {
-    // If the message starts with !hello.. 
+    // If the message starts with !cute.. 
     if (message.indexOf('!cute') === 0) {
-        // Say something 
-        // https://github.com/Schmoopiie/twitch-irc/wiki/Command:-Say 
+        // Tells the user the following message
         client.say(channel, "NO I'M NOT! SwiftRage");
     }
 });
 
-//Displays League of LEgends rank
+//Displays League of Legends rank
 client.addListener('chat', function (channel, user, message) {
-    // If the message starts with !hello.. 
+    //If the message says ONLY !rank
     if (message.indexOf('!rank') === 0) {
+        //Tell the user options for the command
+        cliant.say(channel, "Displays Hay's rank in various games. Useage: '!rank [game]' where [game] is csgo, lol, or osu")
+    }
+    // If the message starts with !rank lol... 
+    if (message.indexOf('!rank lol') === 0) {
         // Say something 
         // https://github.com/Schmoopiie/twitch-irc/wiki/Command:-Say 
-        client.say(channel, "Silver 5 S3, Bronze 2 S4, unranked S5. Now over to Mikuia for Osu! ranking.");
+        client.say(channel, "Silver 5 S3, Bronze 2 S4, unranked so far in S5");
+    }
+});
+
+//Displays osu! rank (rquires Osu! API)
+client.addListener('chat', function (channel, user, message) {
+    // If the message starts with !rank lol... 
+    if (message.indexOf('!rank osu') === 0) {
+        //Displays Osu! global rank, PP, and accuracy
+        client.say(channel, "Osu! stats for Hay1tsme <null>");
+    }
+});
+
+//Displays CS:GO rank (requires Valve API???)
+client.addListener('chat', function (channel, user, message) {
+    // If the message starts with !rank csgo... 
+    if (message.indexOf('!rank csgo') === 0) {
+        //Displays Osu! global rank, PP, and accuracy
+        client.say(channel, "CS:GO stats for Hay1tsme <null>");
     }
 });
 
@@ -65,3 +94,11 @@ client.addListener('chat', function (channel, user, message) {
         client.say(channel, "Sir Hay1tsme Pro's lolking http://www.lolking.net/summoner/na/39879744");
     }
 });
+
+cliant.addListener('chat', function (channel, user, message) {
+    //if the message contains ONLY !whois
+    if (message.indexOf('!whois') === 0) {
+        //Tell the user the options for the command
+        cliant.say(channel, "Displays chat info for a Twitch user currently in chat (not currently opperational). Useage: '!whois [user]' where 'user' is any twitch username currently watching the stream")
+    }
+}
