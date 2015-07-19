20e71978a60305167a2f1916d4747e70a512e59d
Jacob Foster <jfost3@gmail.com>
2015-07-19 01:38:04 -0500

    Combine chat events
    No need to use individual event callbacks per command.

--- HayBot.js
+++ HayBot.js
@@ -27,78 +27,60 @@ client.addListener('join', function (channel, username) {
 client.addListener('disconnected', function (reason) {
     console.log("Disconnected because" + " " + reason);
 });
-//basic !hello command
+
 client.addListener('chat', function (channel, user, message) {
+    var command = message.toLowerCase();
+//Basic !hello command
     // If the message starts with !hello.. 
-    if (message.indexOf('!hello') === 0) {
+    if (command.indexOf('!hello') === 0) {
         // Greet the user
-        client.say(channel, "Sup dude, Hows it going? OpiOP /");
+        client.say(channel, "Sup dude, Hows it going? OpieOP /");
     }
-});
 //Help command
-cliant.addListener('chat', function (channel, user, message) {
     //if the message starts with !help...
-    if (message.indexOf('!help') === 0) {
+    else if (command.indexOf('!help') === 0) {
         //link the user to the Haybot page on Hay1tsme.com
         cliant.say(channel, "Help for HayBot can be found at https://hay1tsme.com/HayBot");
     }
-}
-
 //Love you foxy <3
-client.addListener('chat', function (channel, user, message) {
     // If the message starts with !cute.. 
-    if (message.indexOf('!cute') === 0) {
+    else if (command.indexOf('!cute') === 0) {
         // Tells the user the following message
         client.say(channel, "NO I'M NOT! SwiftRage");
     }
-});
-
 //Displays League of Legends rank
-client.addListener('chat', function (channel, user, message) {
     //If the message says ONLY !rank
-    if (message.indexOf('!rank') === 0) {
+    else if (command.indexOf('!rank') === 0) {
         //Tell the user options for the command
         cliant.say(channel, "Displays Hay's rank in various games. Useage: '!rank [game]' where [game] is csgo, lol, or osu")
     }
     // If the message starts with !rank lol... 
-    if (message.indexOf('!rank lol') === 0) {
+    else if (command.indexOf('!rank lol') === 0) {
         // Say something 
         // https://github.com/Schmoopiie/twitch-irc/wiki/Command:-Say 
         client.say(channel, "Silver 5 S3, Bronze 2 S4, unranked so far in S5");
     }
-});
-
 //Displays osu! rank (rquires Osu! API)
-client.addListener('chat', function (channel, user, message) {
-    // If the message starts with !rank lol... 
-    if (message.indexOf('!rank osu') === 0) {
+    // If the message starts with !rank osu... 
+    else if (command.indexOf('!rank osu') === 0) {
         //Displays Osu! global rank, PP, and accuracy
         client.say(channel, "Osu! stats for Hay1tsme <null>");
     }
-});
-
 //Displays CS:GO rank (requires Valve API???)
-client.addListener('chat', function (channel, user, message) {
     // If the message starts with !rank csgo... 
-    if (message.indexOf('!rank csgo') === 0) {
+    else if (command.indexOf('!rank csgo') === 0) {
         //Displays Osu! global rank, PP, and accuracy
         client.say(channel, "CS:GO stats for Hay1tsme <null>");
     }
-});
-
-client.addListener('chat', function (channel, user, message) {
-    // If the message starts with !hello.. 
-    if (message.indexOf('!lolking') === 0) {
+    // If the message starts with !lolking.. 
+    else if (command.indexOf('!lolking') === 0) {
         // Say something 
         // https://github.com/Schmoopiie/twitch-irc/wiki/Command:-Say 
         client.say(channel, "Sir Hay1tsme Pro's lolking http://www.lolking.net/summoner/na/39879744");
     }
-});
-
-cliant.addListener('chat', function (channel, user, message) {
     //if the message contains ONLY !whois
-    if (message.indexOf('!whois') === 0) {
+    else if (command.indexOf('!whois') === 0) {
         //Tell the user the options for the command
-        cliant.say(channel, "Displays chat info for a Twitch user currently in chat (not currently opperational). Useage: '!whois [user]' where 'user' is any twitch username currently watching the stream")
+        cliant.say(channel, "Displays chat info for a Twitch user currently in chat (not currently operational). Usage: '!whois [user]' where 'user' is any Twitch username currently watching the stream")
     }
 }
